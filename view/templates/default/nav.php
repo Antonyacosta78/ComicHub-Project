a4860bcbaa71b126cc17ae10a9d27aefb13a0e04
Antony Acosta <36846424+Antonyacosta78@users.noreply.github.com>
2018-04-25 13:15:59 -0300

    Movendo menu de usuario para a navbar superior

--- view/templates/default/nav.php
+++ view/templates/default/nav.php
@@ -1,17 +1,31 @@
-<div id="logo" class="navbar navbar-expand-lg navbar-black">      
-    <a href="<?php echo $this->base_url; ?>"><img class="img-logo" src="<?php echo $this->asset; ?>img/logo.png" style="max-height: 50px;"></a>
- </div>
+<div id="logo" class="navbar navbar-black">
+        <a href="<?php echo $this->base_url; ?>">
+            <img class="img-logo" src="<?php echo $this->asset; ?>img/logo.png">
+        </a>
+        <span class="left-content">
+        <?php if(isset($_SESSION['user'])):?>
+        <a href="<?php echo $this->base_url; ?>Profile">
+            <div id="div-img-user">
+                <img class="img-fluid" id="img-user" src="<?php echo $_SESSION['user']['Userimg'];?>" alt="Foto de <?php echo $_SESSION['user']['Username']; ?>" title="Meu Perfil">
+            </div>
+        </a>
+        <?php endif;?>
+        
+        <?php if(!isset($_SESSION['user'])):?>
+        <div class="btn-menu-top">
+            <a style="color: #999999;" role="button" data-toggle="modal" data-target="#loginModal" class="text-right"><i class="fa fa-sign-in"></i></a>&nbsp;
+        </div>
+        <div class="btn-menu-top">
+            <a style="color: #999999;" href="<?php echo $this->base_url; ?>Register" title="Registrar-se" class="text-right"><i class="fa fa-user-plus"></i></a>
+        </div>
+        <?php endif;?>
+        </span>            
+</div>
 <div id="wrapper" class="toggled">
          <!-- Sidebar -->
-         
         <div id="sidebar-wrapper">
             <ul class="sidebar-nav">
                 <br>
-                <?php if(isset($_SESSION['user'])):?>
-                <a href="perfil.html"><div id="div-img-user"><img class="img-fluid" id="img-user" src="<?php echo $_SESSION['user']['Userimg'];?>" alt="Foto de <?php echo $_SESSION['user']['Username']; ?>" 
-					title="Meu Perfil"></div></a>
-                <br>
-                <?php endif;?>
                 <li>
                     <div class="btn-menu" title="Pesquisar Comics"><a href="pesquisacomics.html"><i class="fa fa-search"></i></a></div>
                 </li>
@@ -23,15 +37,7 @@
                 <li>
                     <div class="btn-menu" title="Listas de Leitura"><a href="comicsfavoritas.html"><i class="fa fa-bookmark"></i></a></div>
                 </li>
-                <?php if(!isset($_SESSION['user'])):?>
-                <li>
-                    <div class="btn-menu" title="login"><a style="color: #999999;" role="button" data-toggle="modal" data-target="#loginModal"><i class="fa fa-sign-in"></i></a></div>
-                </li>
                 
-                <li>
-                    <div class="btn-menu" title="Registrar-se"><a href="<?php echo $this->base_url; ?>Register"><i class="fa fa-user-plus"></i></a></div>
-                </li>
-                <?php endif;?>
             </ul>
         </div>
         <!-- /#sidebar-wrapper -->
