bb229a4f7729e7840608171e25388c6504716d20
danielkx25 <36846400+danielkx25@users.noreply.github.com>
2018-05-21 11:20:36 -0300

    Merge with remote

--- view/templates/default/home.php
+++ view/templates/default/home.php
@@ -39,6 +39,7 @@
           <img class="card-img-top" src="http://placehold.it/250x350" alt="Card image cap">
             <div class="card-body">
               <h2 class="card-title text-center">Hanamura Tales</h2>
+              <div class="rating-box"><i class="fa fa-star"></i>&nbsp;&nbsp;4.3</div> 
               <p class="card-text">Uma grande promessa em termos de rpg online se torna um pesadelo para o jovem estudante Shiiro Ikarasu.</p></p>
             </div>
             <div class="card-footer">
@@ -52,6 +53,7 @@
           <img class="card-img-top" src="http://placehold.it/250x350" alt="Card image cap">
             <div class="card-body">
               <h2 class="card-title text-center">BATGIRL VOL. 3</h2>
+              <div class="rating-box"><i class="fa fa-star"></i>&nbsp;&nbsp;3.5</div> 
               <p class="card-text">Batgirl and Nightwing’s feelings for each other have always run deep...</p>
             </div>
             <div class="card-footer">
@@ -65,6 +67,7 @@
           <img class="card-img-top" src="http://placehold.it/250x350" alt="Card image cap">
             <div class="card-body">
               <h2 class="card-title text-center">A queda de Vader</h2>
+              <div class="rating-box"><i class="fa fa-star"></i>&nbsp;&nbsp;5.0</div> 
               <p class="card-text">Darth Vader acaba enfrentando a frota rebelde sozinho e tem seu TIE abatido, derrubando-o em um planeta próximo. </p>
             </div>
             <div class="card-footer">
