566fc99b579fcda513575486e56bc332f025f7de
Antony Acosta <36846424+Antonyacosta78@users.noreply.github.com>
2018-07-01 22:17:51 -0300

    Arrumada a Comics.class.php, ficou bugada depois do merge

--- controller/Comics.class.php
+++ controller/Comics.class.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 class Comics extends Controller{
 
@@ -39,31 +38,8 @@ class Comics extends Controller{
         $this->view->load('readComic',$data);
         $this->view->load('footer');
     }
-    
 
-=======
-<?php
-class Comics extends Controller{
-
-    public function __construct() {
-        parent::__construct();
-        $this->model = new ComicModel();
-    }
-
-    public function search($text = null){
-        $data = [];
-        //$text = (!$text && isset(filter_input(INPUT_GET,"t"))) ? filter_input(INPUT_GET,"t") : $text;
-        //if($text){
-        //    $data['text']=$text; 
-        //    $data['comics']=$this->model->searchComics($text);
-        //}
-        $this->view->load('header');
-        $this->view->load('nav');
-        $this->view->load('pesquisacomics',$data);
-        $this->view->load('footer');
-    }
-    
-    public function insertComic(){
+    public function insert(){
        
         $data=[];//por enquanto
         
@@ -72,27 +48,4 @@ class Comics extends Controller{
         $this->view->load('inserircomic',$data);
         $this->view->load('footer');
     }
-    
-    public function view($id){
-       
-        $data[]="";//por enquanto
-        
-        $this->view->load('header');
-        $this->view->load('nav');
-        $this->view->load('vercomic',$data);
-        $this->view->load('footer');
-    }
-    
-    public function read($id){
-       
-        $data[]="";//por enquanto
-        
-        $this->view->load('header');
-        $this->view->load('nav');
-        $this->view->load('readComic',$data);
-        $this->view->load('footer');
-    }
-    
-
->>>>>>> 9c0d9fcd9c07cdd6dc48c89597c40aeb6f7ad456
 }
